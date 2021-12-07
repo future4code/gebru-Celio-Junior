7c2b13d302f8672d32acbd934285811c08834ec5
junior <celiojunior0110@gmail.com>
2021-12-07 16:29:51 -0300

    :tada: Exercício 2: Concluído

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -10,7 +10,12 @@ function retornaTamanhoArray(array) {
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
- 
+  const arrayInvertido = []
+  for (let i = array.length - 1; i >= 0; i--) {
+    arrayInvertido.push(array[i])
+  }
+
+  return arrayInvertido
 }
 
 // EXERCÍCIO 03
