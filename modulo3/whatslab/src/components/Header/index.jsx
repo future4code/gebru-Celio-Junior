7d6c0e65df8d20d3b3c4c3d9401b65e765a676ca
junior <celiojunior0110@gmail.com>
2022-02-07 22:58:12 -0300

    Fim dos conflitos 2

--- modulo3/whatslab/src/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as Styled from './styled'
-
-const Header = props => (
-    <Styled.Header>
-        <Styled.Title>{props.title}</Styled.Title>
-    </Styled.Header>
-)
-
-export default Header
