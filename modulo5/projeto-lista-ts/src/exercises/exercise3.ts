c22c15e96bcb6b1da86433fa1203aa0964527334
Célio Júnior <celiojunior0110@gmail.com>
2022-05-28 20:15:05 -0300

    Exercício 3 concluído

---
+++ modulo5/projeto-lista-ts/src/exercises/exercise3.ts
@@ -0,0 +1,29 @@
+enum GENRE {
+  ACTION="ação",
+  DRAMA="drama",
+  COMEDY="comédia",
+  ROMANCE="romance",
+  HORROR="terror"
+}
+
+type Movie = {
+  name: string,
+  releaseYear: number,
+  genre: GENRE,
+  score?: number
+}
+
+export const exercise3 = (name: string, releaseYear: number, genre: GENRE, score?: number): Movie => {
+
+  const movieInfo: Movie = {
+    name,
+    releaseYear,
+    genre,
+    score
+  }
+
+  return movieInfo
+
+}
+
+console.log(exercise3('Os miseráveis', 2012, GENRE.DRAMA, 9.3))
