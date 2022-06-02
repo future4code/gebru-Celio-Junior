b3b6a9860a8a456d9db60ad4e9aaad54202174db
Célio Júnior <celiojunior0110@gmail.com>
2022-06-02 17:19:22 -0300

    Exercício 3 concluído

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -22,3 +22,31 @@ type ToDo = {
   title: string,
   completed: boolean
 }
+
+// Exercício 3
+const toDos: ToDo[] = [
+  {
+    userId: 1,
+    id: 1,
+    title: "Fazer exercício 1",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "Fazer exercício 2",
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "Fazer exercício 3",
+    completed: false
+  },
+  {
+    userId: 2,
+    id: 1,
+    title: "Fazer exercício 4",
+    completed: false
+  }
+]
