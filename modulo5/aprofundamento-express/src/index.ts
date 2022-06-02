e30213b4e61e7cfa5d4a5afd21ca7913ae25c01b
Célio Júnior <celiojunior0110@gmail.com>
2022-06-02 17:13:45 -0300

    Exercício 1 concluído

---
+++ modulo5/aprofundamento-express/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+  console.log('Servidor rodando...')
+})
+
+// Exercício 1
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
